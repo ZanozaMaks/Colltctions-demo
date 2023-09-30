35ad7c8e60036fa70e70995c64246044306f5afe
Mak7im <mak7im2003@mail.ru>
2023-10-01 01:05:54 +0300

    2.5

---
+++ src/main/java/exception/EmployeeStorageIsFullException.java
@@ -0,0 +1,5 @@
+package exception;
+
+public class EmployeeStorageIsFullException extends RuntimeException {
+    public EmployeeStorageIsFullException (String message) {super(message);}
+}
--- src/main/java/service/EmployeeService.java
+++ src/main/java/service/EmployeeService.java
@@ -1,6 +1,66 @@
 package service;
+import com.example.Colltctionsdemo.EmployeeController;
+import exception.EmployeeAlreadyAddedException;
+import exception.EmployeeNotFoundException;
+import exception.EmployeeStorageIsFullException;
+import model.Employee;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
-
+@Service
 public class EmployeeService {
+
+    private static final int MAX_SIZE = 2;
+    private final List<Employee> employees = new ArrayList<>();
+
+    public Employee add(String firstName, String lastName) {
+
+        if (employees.size() > MAX_SIZE) {
+            throw new EmployeeStorageIsFullException("Массив сотрудников переополнен.");
+        }
+
+        Employee newEmployee = new Employee(firstName, lastName);
+
+        if (employees.contains(newEmployee)) {
+            throw new EmployeeAlreadyAddedException("Такой сотрудник уже есть.");
+        }
+
+        employees.add(newEmployee);
+
+        return newEmployee;
+    }
+
+    public Employee remove(String firstName, String lastName) {
+        Employee employeeForRemove = new Employee(firstName, lastName);
+
+        if (!employees.contains(employeeForRemove)) {
+            throw new EmployeeNotFoundException("Невозможно удалить. Такого сотрудника не существует.");
+        }
+
+        employees.remove(employeeForRemove);
+        return employeeForRemove;
+    }
+
+    public Employee get(String firstName, String lastName) {
+        Employee employeeForSearch = new Employee(firstName, lastName);
+
+        if (!employees.contains(employeeForSearch)) {
+            throw new EmployeeNotFoundException("Такого сотрудника не существует.");
+        }
+
+        Employee result = null;
+        for (Employee employee : employees) {
+            if (employee.equals(employeeForSearch)) {
+                return employee;
+            }
+        }
+        return result;
+    }
+
+    public List<Employee> getAll() {
+        return employees;
+    }
 }
